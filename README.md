a334edd00c9cfde33477e1c8a18457570ed225c0
Philophreak <philophreak@protonmail.com>
2018-04-15 19:21:07 +0000

    Added readme and unit tests.

---
+++ README.md
@@ -0,0 +1,7 @@
+# et312b
+A Rust library for the ET312B estim serial protocol.
+
+This library provides low-level control over the ET312B estim device by implementing the [ET312B protocol](https://metafetish.github.io/stpihkal/hardware/erostek-et312b.html).
+
+## Current Status
+This library is still in the early stages of development and needs testing. Please file an issue if you encounter any bugs.
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -7,3 +7,32 @@ pub fn checksum(data: &[u8]) -> u8 {
 pub fn encrypt(data: &[u8], key: u8) -> Vec<u8> {
     data.iter().map(|byte| byte ^ key).collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_checksum() {
+        assert_eq!(checksum(&[0]), 0);
+        assert_eq!(checksum(&[1]), 1);
+        assert_eq!(checksum(&[255]), 255);
+
+        assert_eq!(checksum(&[0, 0]), 0);
+        assert_eq!(checksum(&[0, 1]), 1);
+        assert_eq!(checksum(&[1, 0]), 1);
+        assert_eq!(checksum(&[1, 1]), 2);
+        assert_eq!(checksum(&[255, 1]), 0);
+        assert_eq!(checksum(&[255, 255]), 254);
+
+        assert_eq!(checksum(&[255, 255, 1]), 255);
+        assert_eq!(checksum(&[255, 255, 2]), 0);
+    }
+
+    #[test]
+    fn test_encrypt() {
+        assert_eq!(encrypt(&[1, 2, 3], 0), [1, 2, 3]);
+        assert_eq!(encrypt(&[1, 2, 3], 1), [0, 3, 2]);
+        assert_eq!(encrypt(&[1, 2, 3], 0xf0), [0xf1, 0xf2, 0xf3]);
+    }
+}
